f3fa04d61e3f4a3d91797571400b801467b8705f
Rex malebka <rexmalebka@krutt.org>
2020-08-26 00:10:37 -0500

    three imports

--- static/js/Scene.ts
+++ static/js/Scene.ts
@@ -1,11 +1,16 @@
-import * as THREE from "three";
+import * as THREE from "./three/build/three.module.js";
 
 export interface Scene {
 	init: Function
+	floor: THREE.Plane
+	addFloor: Function
+
 }
 
 export const Scene = {
+	floor: 
 	init: function(){
-	
-	}
+
+	},
+	addFloor: function(){}
 }
